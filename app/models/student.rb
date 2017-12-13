75e9e38558446bfdca5ab9435c1c1220674d4f64
alpbourne <alpbourne@gmail.com>
2017-12-13 20:49:38 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,7 +15,7 @@ class Student < ActiveRecord::Base
   has_many :classrooms, through: :classroom_students
 
   def self.search(search)
-    if search.term?
+    if search.present?
       where('name LIKE ?', "%#{search}%")
     else
       self.all
